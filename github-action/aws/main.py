126456ecdb4e736ea6750fadcf1c133483c920de
Mohammad <rezaparvizi1994@gmail.com>
2023-08-25 00:40:14 +0330

    github-action(aws): add DLQ and event bridge

--- github-action/aws/main.py
+++ github-action/aws/main.py
@@ -1,44 +1,45 @@
 from diagrams import Diagram, Cluster, Edge
-from diagrams.aws.network import APIGateway
+from diagrams.aws.network import APIGateway, VPC
 from diagrams.aws.compute import Lambda, EC2
-from diagrams.aws.integration import SimpleQueueServiceSqsQueue
-from diagrams.aws.management import CloudwatchEventTimeBased
+from diagrams.aws.integration import SQS, Eventbridge
+from diagrams.aws.management import Cloudwatch
 from diagrams.aws.storage import SimpleStorageServiceS3
 from diagrams.custom import Custom
 
-with Diagram(outformat="png", filename="github_runner_aws"):
+with Diagram(outformat="png", filename="improved_github_runner_aws"):
     # init custom logos
     cc_github_app = Custom("Github App", "assets/github-logo.jpeg")
 
-    # init scale up components
+    # init webhook components
     gw = APIGateway("API Gateway")
-    webhook = Lambda("Webhook")
-    sqs = SimpleQueueServiceSqsQueue("Webhook Queue")
-    runner_up = Lambda("Scale Runners up")
+    webhook_validator = Lambda("Webhook Validator")
+    sqs = SQS("Webhook Queue")
+    dead_letter_queue = SQS("DLQ")
+    sqs >> dead_letter_queue
 
-    with Cluster("Runners"):
-        servers = [
-            EC2("Runner 1"),
-            EC2("Runner 2"),
-            EC2("Runner 3"),
-        ]
+    with Cluster("VPC"):
+        with Cluster("Auto Scaling"):
+            asg = [EC2("Runner 1"),
+                   EC2("Runner 2"),
+                   EC2("Runner 3")]
 
-    # scale up runners
-    cc_github_app >> \
-    Edge(label="Webhook", color="black") >> gw >> \
-    Edge(label="Post Event", color="black") >> webhook >> \
-    Edge(label="Request run event") >> sqs >> \
-    Edge(label="Send scale up event") >> runner_up >> \
-    Edge(label="Creates", color="black") >> servers[2]
+        binary_updater = Lambda("Update Binary")
+        scale_down = Lambda("Scale Down")
 
-    # init scale down components
-    runner_down = Lambda("Scale Runners down")
-    update_binary = Lambda("Update Binary")
-    scheduler = CloudwatchEventTimeBased("Scheduler")
-    s3 = SimpleStorageServiceS3("Action Runners Binaries")
+        s3 = SimpleStorageServiceS3("Runners Binaries")
+        binary_updater >> s3
+        scale_down >> Edge(label="Evaluates") >> asg[0]
 
-    # scale down runners
-    runner_down >> Edge(label="Terminates", color="black") >> servers[1]
-    scheduler >> runner_down
-    servers[2] >> Edge(label="Download Binary", color="black") >> s3
-    scheduler >> update_binary >> s3
+    event_bridge = Eventbridge("EventBridge")
+    event_bridge >> scale_down
+    event_bridge >> binary_updater
+
+    cloudwatch = Cloudwatch("CloudWatch Logs & Alarms")
+    webhook_validator >> cloudwatch
+    binary_updater >> cloudwatch
+    scale_down >> cloudwatch
+
+    # Event flow
+    cc_github_app >> Edge(label="Webhook", color="black") >> gw >> webhook_validator
+    webhook_validator >> Edge(label="Valid Events") >> sqs
+    sqs >> Edge(label="Trigger Scaling") >> asg[1]
